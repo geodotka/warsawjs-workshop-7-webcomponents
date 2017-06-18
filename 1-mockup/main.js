835c5f77517a6a7b29280f8871125cdd3801e42a
geodotka <dorota.kowalik@21s.pl>
2017-06-18 12:42:15 +0200

    Add connectedCallback method

--- 1-mockup/main.js
+++ 1-mockup/main.js
@@ -4,7 +4,11 @@ class MockupElement extends HTMLElement {
         console.log('MockupElement created!');
         this.shadow = this.attachShadow({ mode: 'open' });    // mode to parametr, który określa, czy nasz komponent udostępnia shadow root w wewnątrz, zawsze ustawiajmy sobie na open, przynajmniej w trybie debugowym
         console.log(this.shadow);
-        this.shadow.innerHTML = document.querySelector('template').innerHTML;
+    }
+
+    connectedCallback() {
+        console.log('connectedCallback');
+        this.shadow.innerHTML = document.querySelector('template').innerHTML;   // funkcja textContent działa podobnie, ale pomija znaczniki
     }
 }
 window.customElements.define('mockup-element', MockupElement);
