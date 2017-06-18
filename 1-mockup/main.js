b45832fd8005fe0ade4dcaa9158384d2a1e1b7a3
geodotka <dorota.kowalik@21s.pl>
2017-06-18 12:33:05 +0200

    Add template to shadow

--- 1-mockup/main.js
+++ 1-mockup/main.js
@@ -4,7 +4,7 @@ class MockupElement extends HTMLElement {
         console.log('MockupElement created!');
         this.shadow = this.attachShadow({ mode: 'open' });    // mode to parametr, który określa, czy nasz komponent udostępnia shadow root w wewnątrz, zawsze ustawiajmy sobie na open, przynajmniej w trybie debugowym
         console.log(this.shadow);
-        this.shadow.innerHTML = 'Jestem cieniem';
+        this.shadow.innerHTML = document.querySelector('template').innerHTML;
     }
 }
 window.customElements.define('mockup-element', MockupElement);
