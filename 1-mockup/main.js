388d768a71378c665ec9f3b1652e6b4f6e5bf5da
geodotka <dorota.kowalik@21s.pl>
2017-06-18 12:30:06 +0200

    Add shadow to component

--- 1-mockup/main.js
+++ 1-mockup/main.js
@@ -1,7 +1,9 @@
 class MockupElement extends HTMLElement {
     constructor() {
         super();
-        console.log('MockupElement created!')
+        console.log('MockupElement created!');
+        this.shadow = this.attachShadow({ mode: 'open' });    // mode to parametr, który określa, czy nasz komponent udostępnia shadow root w wewnątrz, zawsze ustawiajmy sobie na open, przynajmniej w trybie debugowym
+        console.log(this.shadow)
     }
 }
 window.customElements.define('mockup-element', MockupElement);
