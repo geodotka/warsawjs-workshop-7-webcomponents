8b38a049c50fd3117b157d44ee0f4559d9c51f18
geodotka <dorota.kowalik@21s.pl>
2017-06-18 12:53:15 +0200

    Get url from image attribute of component

--- 1-mockup/main.js
+++ 1-mockup/main.js
@@ -3,12 +3,12 @@ class MockupElement extends HTMLElement {
         super();
         console.log('MockupElement created!');
         this.shadow = this.attachShadow({ mode: 'open' });    // mode to parametr, który określa, czy nasz komponent udostępnia shadow root w wewnątrz, zawsze ustawiajmy sobie na open, przynajmniej w trybie debugowym
-        console.log(this.shadow);
     }
 
     connectedCallback() {
         console.log('connectedCallback');
         this.shadow.innerHTML = document.querySelector('template').innerHTML;   // funkcja textContent działa podobnie, ale pomija znaczniki
+        this.shadow.querySelector('img').src = this.attributes.image.value;
     }
 }
 window.customElements.define('mockup-element', MockupElement);
